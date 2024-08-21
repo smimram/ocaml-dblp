ee13d14cee1ef4bea5166be54e600ea5e7e2844e
Samuel Mimram <smimram@gmail.com>
2024-08-21 10:45:37 +0200

    Update opam.

--- query/dblp.ml
+++ query/dblp.ml
@@ -1,7 +1,2 @@
 let () =
-  (* print_string (DBLP.download "https://www.google.fr") *)
-  (* let s = DBLP.query `Author "Mimram" in *)
-  (* let s = DBLP.query `Publication "Mimram" in *)
-  (* print_string s *)
-
-  let g = DBLP.author "Girard" in
+  ()
